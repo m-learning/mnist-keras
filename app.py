6e8d8a90709d93f5a6d55467655e061ab097aebb
Davit Abulashvili <datobula@gmail.com>
2017-01-28 21:01:33 +0400

    flask

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+from use_mnist import recognize_image
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        score = recognize_image(request.data)
+        print score
+        return str(score[0])
+    else:
+        return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
--- use_mnist.py
+++ use_mnist.py
@@ -1,19 +1,33 @@
+from time import sleep
+
 from keras.models import load_model
 from scipy import misc
 
+
 model = load_model('model/mnist.h5')
 img_rows, img_cols = 28, 28
+image_file = "image.png"
 
 
-def recognize_image(data):
-    image_file = "image.png"
+def _save_image(data):
     with open(image_file, 'w') as fh:
         fh.write(data)
+        fh.close()
 
+
+def _get_score():
     face = misc.imread(image_file, flatten=True, mode='L')
-    for i in xrange(28):
-        for j in xrange(28):
+    for i in xrange(img_rows):
+        for j in xrange(img_cols):
             face[i][j] = 255 - face[i][j]
     image = face.reshape(1, img_rows, img_cols, 1)
     image = image.astype('float32')
     return model.predict_classes(image)
+
+
+def recognize_image(data):
+    _save_image(data)
+    return _get_score()
+
+if __name__ == '__main__':
+    print _get_score()
